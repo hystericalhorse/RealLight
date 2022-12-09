b6d693b4b3fdfdd2c5f24e1dcaa356812ec2d793
Birdy Bird <edixon@student.neumont.edu>
2022-12-09 13:53:51 -0700

    Added some extra scenes.

--- RealLight/Run/Main.cpp
+++ RealLight/Run/Main.cpp
@@ -87,12 +87,364 @@ void Marbles(RealLight::Scene& scene)
 	}
 }
 
+void Tube(RealLight::Scene& scene)
+{
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, -5, 0 }, 2.0f, std::make_unique<RealLight::Lambert>(color3{ 1, 0, 0 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 5, 0 }, 2.0f, std::make_unique<RealLight::Lambert>(color3{ 0, 1, 0 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 4, 3, 0 }, 2.0f, std::make_unique<RealLight::Lambert>(color3{ 0, 0, 1 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 4, -3, 0 }, 2.0f, std::make_unique<RealLight::Metal>(color3{ 1 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -4, 3, 0 }, 2.0f, std::make_unique<RealLight::Dielectric>(color3{ 1 }, 0.5)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -4, 3, 0 }, 2.0f, std::make_unique<RealLight::Emissive>(color3{ 1000 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -4, -3, 0 }, 2.0f, std::make_unique<RealLight::Lambert>(color3{ 0, 1, 0 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -4, 0, -5 }, 2.0f, std::make_unique<RealLight::Lambert>(color3{ 0, 0, 1 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 4, 0, -5 }, 2.0f, std::make_unique<RealLight::Dielectric>(color3{ 1 }, 0.5)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 4, 0, -5 }, 2.0f, std::make_unique<RealLight::Emissive>(color3{ 1000 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 2.1, 4, -5 }, 2.0f, std::make_unique<RealLight::Lambert>(color3{ 1, 0, 0 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 2.1, -4, -5 }, 2.0f, std::make_unique<RealLight::Lambert>(color3{ 0, 1, 0 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -2.1, 4, -5 }, 2.0f, std::make_unique<RealLight::Metal>(color3{ 1 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -2.1, -4, -5 }, 2.0f, std::make_unique<RealLight::Dielectric>(color3{ 1 }, 0.5)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -2.1, -4, -5 }, 2.0f, std::make_unique<RealLight::Emissive>(color3{ 1000 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, -5, -9 }, 2.0f, std::make_unique<RealLight::Metal>(color3{ 1 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 5, -9 }, 2.0f, std::make_unique<RealLight::Dielectric>(color3{ 1 }, 0.5)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 5, -9 }, 1.0f, std::make_unique<RealLight::Emissive>(color3{ 1000 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 4, 3, -9 }, 2.0f, std::make_unique<RealLight::Lambert>(color3{ 0, 0, 1 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 4, -3, -9 }, 2.0f, std::make_unique<RealLight::Metal>(color3{ 1 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -4, 3, -9 }, 2.0f, std::make_unique<RealLight::Metal>(color3{ 1, 0, 0 })
+			)
+	);
+
+	// Emissives
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -20, 0, 0 }, 0.2f, std::make_unique<RealLight::Emissive>(color3{ 100000, 0, 100000 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 20, 0, -5 }, 0.2f, std::make_unique<RealLight::Emissive>(color3{ 0, 100000, 100000 })
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 20, -9 }, 0.2f, std::make_unique<RealLight::Emissive>(color3{ 1000, 1000, 100000 })
+			)
+	);
+
+	
+}
+
+void Circles(RealLight::Scene& scene)
+{
+	scene.addObject
+	(
+		std::make_unique<RealLight::Plane>
+		(
+			glm::vec3{ 0, -1, 0 }, glm::vec3{ 0, 1, 0 }, std::make_unique<RealLight::Lambert>(color3{ 0.2 })
+			)
+	);
+
+
+	// Inner Circle
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 3, 0, 3 }, 1.0f, std::make_unique<RealLight::Metal>(color3{ 0, 1, 1 }, 0.1)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 4, 0, 0 }, 1.0f, std::make_unique<RealLight::Metal>(color3{ 0, 0, 1 }, 0.1)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 3, 0, -3 }, 1.0f, std::make_unique<RealLight::Metal>(color3{ 1, 0, 1 }, 0.1)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 0, -4 }, 1.0f, std::make_unique<RealLight::Metal>(color3{ 0, 1, 0 }, 0.1)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -3, 0, -3 }, 1.0f, std::make_unique<RealLight::Metal>(color3{ 1, 1, 0 }, 0.1)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -4, 0, 0 }, 1.0f, std::make_unique<RealLight::Metal>(color3{ 1, 0, 0 }, 0.1)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -3, 0, 3 }, 1.0f, std::make_unique<RealLight::Metal>(color3{ 0.6, 1, 0.4 }, 0.1)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 0, 4 }, 1.0f, std::make_unique<RealLight::Metal>(color3{ 1, 0.4, 0.6 }, 0.1)
+			)
+	);
+
+	// Outer Circle ///////////////////////////////////////////////////////////////////////////////////////
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 12, 4, 12 }, 4.0f, std::make_unique<RealLight::Dielectric>(color3{ 1, 0.4, 0.6 }, 2.0)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 16, 4, 0 }, 4.0f, std::make_unique<RealLight::Dielectric>(color3{ 0, 1, 1 }, 2.0)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 12, 4, -12 }, 4.0f, std::make_unique<RealLight::Dielectric>(color3{ 0, 0, 1 }, 2.0)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 4, -16 }, 4.0f, std::make_unique<RealLight::Dielectric>(color3{ 1, 0, 1 }, 2.0)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -12, 4, -12 }, 4.0f, std::make_unique<RealLight::Dielectric>(color3{ 0, 1, 0 }, 2.0)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -16, 4, 0 }, 4.0f, std::make_unique<RealLight::Dielectric>(color3{ 1, 1, 0 }, 2.0)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ -12, 4, 12 }, 4.0f, std::make_unique<RealLight::Dielectric>(color3{ 1, 0, 0 }, 2.0)
+			)
+	);
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 4, 16 }, 4.0f, std::make_unique<RealLight::Dielectric>(color3{ 0.6, 1, 0.4 }, 2.0)
+			)
+	);
+
+
+	// Skylight ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	scene.addObject
+	(
+		std::make_unique<RealLight::Sphere>
+		(
+			glm::vec3{ 0, 200, 0 }, 100.0f, std::make_unique<RealLight::Emissive>(color3{ 2 })
+			)
+	);
+
+}
+
 int main(int, char**)
 {
-	const int width		= 800;
-	const int height	= 400;
+	const int width		= 1280;
+	const int height	= 720;
 
-	const int samples	= 5;
+	const int samples	= 1;
 
 	const float fov = 40.0f;
 
@@ -102,13 +454,15 @@ int main(int, char**)
 	renderer.Init();
 	renderer.CreateWindow(width, height);
 
-	RealLight::Scene scene({ 0.7, 0.2, 0.8 } , {1, 1, 1});
+	RealLight::Scene scene({ 0.7, 0.2, 0.8 }, { 1, 1, 1 });
 
 	RealLight::Canvas canvas(width, height, renderer);
 
-	Default(scene);
+	Circles(scene);
 
-	RealLight::Camera camera({ 13, 2, 3 }, { 0, 0, 0 }, { 0, 1, 0 }, fov, width / (float) height);
+	RealLight::Camera camera({ 0, 40, 48 }, { 0, 0, 0 }, { 0, 1, 0 }, fov, width / (float) height);
+	//RealLight::Camera camera({ 0, 25, 0 }, { 0, 0, 0 }, { 0, 1, 0 }, fov, width / (float) height);
+	//RealLight::Camera camera({ 13, 2, 3 }, { 0, 0, 0 }, { 0, 1, 0 }, fov, width / (float) height);
 
 	// Run
 	bool quit = false;
